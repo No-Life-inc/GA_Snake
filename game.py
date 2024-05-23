bc1ecf60f1e8d28f0d374f51f4c6235dd9789940
Morten Bendeke <mbendeke@hotmail.com>
2024-05-23 10:39:25 +0200

    pytorch seed

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import random
 from snake import Snake
 from ga_brain import GABrain
 import pickle
+import torch
 
 class SnakeGame:
     def __init__(self, brain=None, width=800, height=600, snake_size=20, display=False):
@@ -291,8 +292,8 @@ class SnakeGame:
         return snake_age, score, food_eaten
 
     def generate_food(self):
-        return  [random.randrange(self.WIDTH // self.SNAKE_SIZE) * self.SNAKE_SIZE,
-                         random.randrange(self.HEIGHT // self.SNAKE_SIZE) * self.SNAKE_SIZE]
+        return  [int(torch.randint(0, self.WIDTH // self.SNAKE_SIZE, (1,)).item()) * self.SNAKE_SIZE,
+                int(torch.randint(0, self.HEIGHT // self.SNAKE_SIZE, (1,)).item()) * self.SNAKE_SIZE]
 
     def wall_collide(self, pos):
         """Check if a given position collides with the wall."""
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from pytorch.pytorch_ga_factory import GeneticAlgorithmTorch
 from selection_methods import alpha_selection, tournament_selection, elitism_selection, rank_selection, roulette_wheel_selection, top_20_percent
 from crossover_methods import single_point_crossover, two_point_crossover, uniform_crossover, arithmetic_crossover
 import pygame
+import torch
 
 
 if __name__ == "__main__":
 # make combinations of crossovers selection methods and population sizes and mutation rates and run the genetic algorithm
+
+    seeds = range(1)
 
     population_sizes = [2000]
     mutation_rates = [0.1]
@@ -14,7 +17,9 @@ if __name__ == "__main__":
     crossover_methods = [single_point_crossover, two_point_crossover, arithmetic_crossover]
     generations = 100
     elistism_rates = [0.1]
-
+    
+for seed in seeds:
+    torch.manual_seed(seed)
     for population_size in population_sizes:
         for mutation_rate in mutation_rates:
             for selection_method in selection_methods:
