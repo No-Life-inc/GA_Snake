fe4e1b11a0e622c24d89767eec78023502f12e25
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-05-27 15:24:31 +0200

    edited text on plot

--- plotting_media_plot_mut.py
+++ plotting_media_plot_mut.py
@@ -14,20 +14,27 @@ class GraphMaker:
 
         plt.figure(figsize=(12, 8))
 
+        
+
         for json_file in json_files:
             # Step 1: Read the JSON file
             df = pd.read_json(json_file)
-
+            label = os.path.basename(json_file)
+            label_parts = label.split('_')  # Assuming the parts of the label are separated by underscores
+            short_label = '_'.join(label_parts[:5])  # Only keep the first five parts
             # Step 2: Calculate the median
             median = df.median()
 
             # Step 3: Plot the median
-            plt.plot(median, label=os.path.basename(json_file))
+            plt.plot(median, label=short_label)
 
-        plt.title('Median of Data from JSON Files')
-        plt.xlabel('Columns')
+        plt.title('Performance of crossovers/selections across parameter combinations', y=1.15)
+        plt.xlabel('Generations')
         plt.ylabel('Median')
         plt.legend()
+
+        plt.text(0.10, 0.98, '- Random Mutation\n- All Population\n- 0.05 & 0.1 Mutation Rate\n- 0.1 Elitism', transform=plt.gcf().transFigure, ha='left', va='top')
+        plt.legend(bbox_to_anchor=(0.5, 1.15), loc='upper center', ncol=2, fontsize='small')
 
         # Set the x-axis limit
         plt.xlim(0, 50)
